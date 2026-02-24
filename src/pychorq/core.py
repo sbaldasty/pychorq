1c80e55e66550ab0f4abb858629637703a62250c
Steven Baldasty <sbaldasty@gmail.com>
2026-02-24 13:52:00 -0500

    ownership for classical messages

--- src/pychorq/core.py
+++ src/pychorq/core.py
@@ -8,66 +8,19 @@ from qiskit.circuit import QuantumRegister
 class LocalQuantumBackend(LocalBackend):
 
     def __init__(self):
+        super().__init__()
         self.views = defaultdict(list)
 
-    def _validate_qubit_located_value(self, lv):
-        assert isinstance(lv, LocatedVal), f'Expected LocatedVal, got {type(lv)}'
-        assert len(lv.parties) == 1, f'Quantum values must have exactly one owner: {lv}'
-        assert isinstance(lv.val, QuantumRegister), f'Backend only supports quantum registers: {type(lv.val)}'
-
     def send(self, party_from, party_to, lv, note=None):
+        assert isinstance(lv, LocatedVal), f'Expected LocatedVal, got {type(lv)}'
         assert isinstance(party_from, Party)
         assert isinstance(party_to, Party)
-        self._validate_qubit_located_value(lv)
-        assert party_from in lv.parties, f'{party_from} is not owner of {lv}'
+        assert party_from in lv.parties
 
-        val = self.unwrap(lv, {party_from})
-        self.views[party_to].append(val)
+        if not isinstance(lv.val, QuantumRegister):
+            return super().send(party_from, party_to, lv, note)
 
+        qr = self.unwrap(lv, {party_from})
+        self.views[party_to].append(qr)
         lv.parties.clear()
         lv.parties.add(party_to)
-
-    def locally(self, f, *args, **kwargs):
-        new_args, new_parties = get_val(args)
-        output = f(*new_args)
-        out = LocatedVal(new_parties.copy(), output)
-        self._validate_qubit_located_value(out)
-        return out
-
-    def unwrap(self, lv, p):
-        self._validate_qubit_located_value(lv)
-        if p.issubset(lv.parties):
-            return lv.val
-        return None
-
-    def unlist(self, ls, length):
-        self._validate_qubit_located_value(ls)
-        raise Exception('QuantumQubitBackend does not support unlist for single-qubit values')
-
-    def untup(self, ls, length):
-        self._validate_qubit_located_value(ls)
-        raise Exception('QuantumQubitBackend does not support untup for single-qubit values')
-
-    def undict(self, d, keys):
-        self._validate_qubit_located_value(d)
-        raise Exception('QuantumQubitBackend does not support undict for single-qubit values')
-
-# TODO Copied from pychor for now, this might need to change for qubits
-def get_val(lv):
-    if isinstance(lv, LocatedVal):
-        return cc.unwrap(lv, lv.parties), lv.parties
-    elif isinstance(lv, (tuple, list)):
-        vals, parties_ls = zip(*[get_val(x) for x in lv])
-        parties_setlist = [p for p in parties_ls if p is not None]
-        assert len(parties_setlist) > 0, f'No party information for {lv}'
-        parties = set.intersection(*parties_setlist)
-        assert len(parties) > 0, f'No participating parties for {lv}'
-        return vals, parties
-    # elif isinstance(lv, (dict)):
-    #     return {get_val(k, party): get_val(v, party) for k, v in lv.items()}
-    elif isinstance(lv, (int, float, str)):
-        return lv, None
-    # else:
-    #     return lv
-    else:
-        raise Exception(f'Unsupported value for local computation: {lv} : {type(lv)}')
--- tests/test_core.py
+++ tests/test_core.py
@@ -5,9 +5,9 @@ from pytest import raises
 from qiskit.circuit import QuantumRegister
 
 
-def test_single_qubit_ownership():
+def test_qubit_ownership():
     '''
-    Qubits have exactly one owner at a time. Sending a qubit transfers
+    Qubits have exactly one owner at a time. Sending qubits transfers
     ownership. Parties can only send qubits they own.
     '''
     alice = Party('Alice')
@@ -23,3 +23,24 @@ def test_single_qubit_ownership():
         # Alice should not be able to send the qubit anymore
         with raises(expected_exception=AssertionError):
             loc_q.send(src=alice, dest=bob)
+
+
+def test_classical_ownership():
+    '''
+    Classical values can have multiple owners. Sending classical adds to the
+    set of owners. Parties can only send classical values they own.
+    '''
+    alice = Party('Alice')
+    bob = Party('Bob')
+    charlie = Party('Charlie')
+    val = 42
+    with LocalQuantumBackend():
+        loc_val = val@alice
+        # Owner should be exlusively Alice
+        assert loc_val.parties == {alice}
+        loc_val.send(src=alice, dest=bob)
+        # Alice and Bob should both be owners
+        assert loc_val.parties == {alice, bob}
+        # Charlie does not own the value and should not be able to send it
+        with raises(expected_exception=AssertionError):
+            loc_val.send(src=charlie, dest=bob)
